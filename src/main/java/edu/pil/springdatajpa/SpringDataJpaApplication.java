b698c85b34f0d974c95a27ea761f331dc90e4896
Petro Lisnyi <petro.lisnyi@gmail.com>
2023-12-29 14:25:14 +0200

    App with postgres db

---
+++ src/main/java/edu/pil/springdatajpa/SpringDataJpaApplication.java
@@ -0,0 +1,16 @@
+package edu.pil.springdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class SpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/edu/pil/springdatajpa/domain/Book.java
@@ -0,0 +1,48 @@
+package edu.pil.springdatajpa.domain;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "books" /*, schema = "my_schema"*/)
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String isbn;
+    private String publisher;
+
+    public Book() {
+    }
+
+    public Book(String title, String isbn, String publisher) {
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+}
+
