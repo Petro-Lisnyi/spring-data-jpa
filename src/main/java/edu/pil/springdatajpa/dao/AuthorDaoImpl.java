5bbb93eda05a7254507192ffd6deb9f7d7143e5f
Petro Lisnyi <petro.lisnyi@gmail.com>
2024-01-25 11:27:30 +0200

    Use PreparedStatement

--- src/main/java/edu/pil/springdatajpa/dao/AuthorDaoImpl.java
+++ src/main/java/edu/pil/springdatajpa/dao/AuthorDaoImpl.java
@@ -4,10 +4,7 @@ import edu.pil.springdatajpa.domain.Author;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 @Component
 public class AuthorDaoImpl implements AuthorDao {
@@ -21,13 +18,14 @@ public class AuthorDaoImpl implements AuthorDao {
     @Override
     public Author getById(Long id) {
         Connection connection = null;
-        Statement statement = null;
+        PreparedStatement ps = null;
         ResultSet resultSet = null;
 
         try {
             connection = dataSource.getConnection();
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery("SELECT * FROM author WHERE id = " + id);
+            ps = connection.prepareStatement("SELECT * FROM author WHERE id = ?");
+            ps.setLong(1, id);
+            resultSet = ps.executeQuery();
 
             if (resultSet.next()) {
                 var author = new Author();
@@ -42,8 +40,8 @@ public class AuthorDaoImpl implements AuthorDao {
             try {
                 if (connection != null)
                     connection.close();
-                if (statement != null)
-                    statement.close();
+                if (ps != null)
+                    ps.close();
                 if (resultSet != null)
                     resultSet.close();
             } catch (SQLException e) {
