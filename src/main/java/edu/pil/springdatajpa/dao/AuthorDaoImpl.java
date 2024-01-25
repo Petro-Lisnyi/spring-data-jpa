16936a70a6f3b5a0ab442e953362ed513cabc6ee
Petro Lisnyi <petro.lisnyi@gmail.com>
2024-01-25 11:55:36 +0200

    Refactoring duplicate code

--- src/main/java/edu/pil/springdatajpa/dao/AuthorDaoImpl.java
+++ src/main/java/edu/pil/springdatajpa/dao/AuthorDaoImpl.java
@@ -27,23 +27,14 @@ public class AuthorDaoImpl implements AuthorDao {
             ps.setLong(1, id);
             resultSet = ps.executeQuery();
 
-            if (resultSet.next()) {
-                var author = new Author();
-                author.setId(id);
-                author.setFirstName(resultSet.getString("first_name"));
-                author.setLastName(resultSet.getString("last_name"));
-                return author;
-            }
+            if (resultSet.next())
+                return getAuthorFromResultSet(resultSet);
+
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
             try {
-                if (connection != null)
-                    connection.close();
-                if (ps != null)
-                    ps.close();
-                if (resultSet != null)
-                    resultSet.close();
+                closeAll(connection, ps, resultSet);
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -64,27 +55,35 @@ public class AuthorDaoImpl implements AuthorDao {
             ps.setString(2, lastName);
             resultSet = ps.executeQuery();
 
-            if (resultSet.next()) {
-                var author = new Author();
-                author.setId(resultSet.getLong("id"));
-                author.setFirstName(resultSet.getString("first_name"));
-                author.setLastName(resultSet.getString("last_name"));
-                return author;
-            }
+            if (resultSet.next())
+                return getAuthorFromResultSet(resultSet);
+
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
             try {
-                if (connection != null)
-                    connection.close();
-                if (ps != null)
-                    ps.close();
-                if (resultSet != null)
-                    resultSet.close();
+                closeAll(connection, ps, resultSet);
             } catch (SQLException e) {
                 e.printStackTrace();
             }
         }
         return null;
+    }
+
+    private Author getAuthorFromResultSet(ResultSet resultSet) throws SQLException {
+        var author = new Author();
+        author.setId(resultSet.getLong("id"));
+        author.setFirstName(resultSet.getString("first_name"));
+        author.setLastName(resultSet.getString("last_name"));
+        return author;
+    }
+
+    private void closeAll(Connection connection, PreparedStatement ps, ResultSet resultSet) throws SQLException {
+        if (connection != null)
+            connection.close();
+        if (ps != null)
+            ps.close();
+        if (resultSet != null)
+            resultSet.close();
     }
 }
