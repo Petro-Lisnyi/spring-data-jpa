6db9034b68d8808b871821c5cb4a4339879bdb1d
Petro Lisnyi <petro.lisnyi@gmail.com>
2024-01-26 00:31:50 +0200

    Add integration tests for BookDao

---
+++ src/test/java/edu/pil/springdatajpa/BookDaoIntegrationTest.java
@@ -0,0 +1,67 @@
+package edu.pil.springdatajpa;
+
+import edu.pil.springdatajpa.dao.BookDao;
+import edu.pil.springdatajpa.dao.BookDaoImpl;
+import edu.pil.springdatajpa.domain.Author;
+import edu.pil.springdatajpa.domain.Book;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@ActiveProfiles("local")
+@DataJpaTest
+@Import(BookDaoImpl.class)
+//@ComponentScan(basePackages = {"edu.pil.springdatajpa.dao"})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class BookDaoIntegrationTest {
+
+    @Autowired
+    BookDao bookDao;
+
+    @Test
+    void getBookTest() {
+        var book = bookDao.getById(1L);
+        assertThat(book).isNotNull();
+    }
+
+    @Test
+    void getBookByTitleTest() {
+        var book = bookDao.findBookByTitle("Domain-Driven Design");
+        System.out.println("book_id = " + book.getId());
+        assertThat(book).isNotNull();
+    }
+
+    @Test
+    void saveBookTest() {
+        var savedBook = bookDao.saveBook(new Book("Harnessing Hibernate",
+                "978-0-596-51772-4", "James Elliot, Tim O'Brien"));
+        assertThat(savedBook).isNotNull();
+        bookDao.deleteBookById(savedBook.getId());
+    }
+
+    @Test
+    void updateBookTest() {
+        var book = new Book("English For Tech", "anna_gandrabura", "Anna Gandrabura");
+
+        var savedBook = bookDao.saveBook(book);
+        savedBook.setIsbn("annglish_");
+
+        var updatedBook = bookDao.updateBook(savedBook);
+        assertThat(updatedBook.getIsbn()).isEqualTo("annglish_");
+        bookDao.deleteBookById(updatedBook.getId());
+    }
+
+    @Test
+    void deleteBookTest() {
+        var saved = bookDao.saveBook(new Book());
+        bookDao.deleteBookById(saved.getId());
+
+        var deleted = bookDao.getById(saved.getId());
+        assertThat(deleted).isEqualTo(null);
+    }
+}
