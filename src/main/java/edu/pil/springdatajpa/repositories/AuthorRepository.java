3a239c14b0c306419cac975cf786c6c3f6eacb74
Petro Lisnyi <petro.lisnyi@gmail.com>
2024-02-05 20:53:47 +0200

    Add a Query method

--- src/main/java/edu/pil/springdatajpa/repositories/AuthorRepository.java
+++ src/main/java/edu/pil/springdatajpa/repositories/AuthorRepository.java
@@ -5,4 +5,5 @@ import edu.pil.springdatajpa.domain.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+    Author findAuthorByFirstNameAndLastName(String firstName, String lastName);
 }
